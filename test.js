020d025075777777073f39559988f355df1f609e
tareqferdous <16101017@uap-bd.edu>
2025-05-29 07:07:57 +0600

    first commit

---
+++ test.js
@@ -0,0 +1,8 @@
+console.log("I am a test script");
+
+function init() {
+  document.getElementById("someId").innerText =
+    "This is the first lesson of 40 days of Javascript course";
+}
+
+init();
