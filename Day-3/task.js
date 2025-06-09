24e241a34d3fffc550093af94b31f8fbba058dc3
tareqferdous <16101017@uap-bd.edu>
2025-06-09 08:37:56 +0600

    task complete

--- Day-3/task.js
+++ Day-3/task.js
@@ -14,15 +14,53 @@ console.log(
 // task 3
 
 let monthlySalary = 12300;
-let lastMonthBonus = (20 / 100) * monthlySalary;
-const lastMonthSalary = monthlySalary + lastMonthBonus;
+const annualSalary = monthlySalary * 12;
+const bonus = (20 / 100) * annualSalary;
+const totalAnnualSalary = annualSalary + bonus;
 
-const annualSalary = lastMonthSalary + monthlySalary * 11;
-console.log(
-  `The annual salary is ${annualSalary} and the last month salary is ${lastMonthSalary}`
-);
+console.log(`The annual salary is ${totalAnnualSalary}`);
 
 // TODO Question 4:
 
 const color = "Red";
 console.log(color === "Red" ? "STOP" : "GO");
+
+// TODO Question 5:
+
+const unitsPerDay = 10;
+const unitCost = 150;
+
+const monthlyBill = unitsPerDay * 30 * unitCost;
+const annualBill = monthlyBill * 12;
+const discount = (20 / 100) * annualBill;
+const annualBillWithDiscount = annualBill - bonus;
+console.log(annualBillWithDiscount);
+
+// TODO Question 6:
+
+const year = 2025;
+console.log(year % 4 === 0 ? "Leap Year" : "Not a leap Year");
+
+// TODO Question 7:
+const p = 10;
+const q = 20;
+const r = 30;
+console.log(
+  p > q && p > r
+    ? "P is greater"
+    : q > p && q > r
+    ? "Q is greater"
+    : "R is greater"
+);
+
+// TODO Question 8:
+const count = 5;
+/** convert it into binary"
+ *  5/2 = 2 remainer(1)
+ * 2/2 = 1 remainder(0)
+ * 1/2 = 0 remaineder (1)
+ *  the binary of 5 = 101
+ *
+ * 1*(2**0)+0*(2**1)+ 1*(2**2)
+ * **/
+console.log(count << 1);
